5034edf8e2947cbb1f72d698317ecfd62053f32e
Thomas Modeneis <thomas.modeneis@gmail.com>
2015-05-31 16:41:21 +0200

    #1

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	p := os.Getenv("PORT")
+	if p == "" {
+		p = "3006"
+	}
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
 
-	log.Println("Listening...")
-	http.ListenAndServe(":3006", nil)
+	log.Println("Listening -> "+p)
+	http.ListenAndServe(":"+p, nil)
 }
---
+++ readme.md
@@ -0,0 +1,12 @@
+Description
+===========
+
+This is the web project for (JGO)[https://github.com/thomasmodeneis/jgo].
+
+License
+=======
+
+GNU 3
+-----
+
+GNU General Public License, version 3.
